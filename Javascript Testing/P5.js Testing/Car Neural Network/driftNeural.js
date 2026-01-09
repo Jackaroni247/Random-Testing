f9e6f6eb11a0abd1899562e674b2a7d3acaa675e
Jackaroni <121079788+Jackaroni247@users.noreply.github.com>
2026-01-09 12:40:17 -0600

    Added a track

--- Javascript Testing/P5.js Testing/Car Neural Network/driftNeural.js
+++ Javascript Testing/P5.js Testing/Car Neural Network/driftNeural.js
@@ -1,13 +1,24 @@
-var car1 = new car();
+var car1 = new car(250, 800/2);
+
 
 function setup() {
 createCanvas(1000,800);
-background(13, 248, 252);
+background(186, 240, 255);
 }
 
 function draw() {
-background(13, 248, 252);
+background(186, 240, 255);
+
 car1.tick();
 car1.draw();
 
+drawTrack();
+
+}
+
+function drawTrack() {
+    strokeWeight(20);
+    noFill();
+    circle(500,400,300);
+    circle(500,400,700);
 }
