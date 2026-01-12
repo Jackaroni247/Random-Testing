d68932ed4610f7e27f91effa50c0bdda6b767f0d
Jackaroni <121079788+Jackaroni247@users.noreply.github.com>
2026-01-12 08:50:15 -0600

    Made this jibe with changes to car class

--- Javascript Testing/P5.js Testing/Car Neural Network/driftNeural.js
+++ Javascript Testing/P5.js Testing/Car Neural Network/driftNeural.js
@@ -1,5 +1,6 @@
 var car1 = new car(250, 800/2);
-
+var ringIN = 300;
+var ringOUT = 700;
 
 function setup() {
 createCanvas(1000,800);
@@ -9,8 +10,10 @@ background(186, 240, 255);
 function draw() {
 background(186, 240, 255);
 
-car1.tick();
-car1.draw();
+if(!car1.dead) {
+    car1.tick();
+    car1.draw();
+}
 
 drawTrack();
 
@@ -19,6 +22,6 @@ drawTrack();
 function drawTrack() {
     strokeWeight(20);
     noFill();
-    circle(500,400,300);
-    circle(500,400,700);
+    circle(500,400,ringIN);
+    circle(500,400,ringOUT);
 }
