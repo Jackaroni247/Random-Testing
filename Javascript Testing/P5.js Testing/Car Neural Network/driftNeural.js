30225954f33026d2fb7922ae6647da2b25fdfd1b
Jackaroni <121079788+Jackaroni247@users.noreply.github.com>
2026-01-13 21:40:39 -0600

    Set up support for multi car runs to support generations

--- Javascript Testing/P5.js Testing/Car Neural Network/driftNeural.js
+++ Javascript Testing/P5.js Testing/Car Neural Network/driftNeural.js
@@ -1,27 +1,56 @@
-var car1 = new car(250, 800/2);
 var ringIN = 300;
 var ringOUT = 700;
 
+var Cars = [];
+var numCars = 10;
+
 function setup() {
-createCanvas(1000,800);
-background(186, 240, 255);
+    createCanvas(1000, 800);
+    background(186, 240, 255);
+
+    //Goes through and creates all of the car objects;
+    for (var i = 0; i < numCars; i++) {
+        Cars[i] = new Car(250, 400);
+    }
 }
 
 function draw() {
-background(186, 240, 255);
+    background(186, 240, 255);
 
-if(!car1.dead) {
-    car1.tick();
-    car1.draw();
+    //Ticks to check for user input
+    controlTick();
+
+    //Goes through each car and simulates and draws
+    for (var i = 0; i < numCars; i++) {
+        if (!Cars[i].dead) {
+            Cars[i].tick();
+            Cars[i].draw();
+        }
+    }
+
+    //Draws the track
+    drawTrack();
 }
 
-drawTrack();
-
-}
-
+    //Draws the track
 function drawTrack() {
     strokeWeight(20);
     noFill();
-    circle(500,400,ringIN);
-    circle(500,400,ringOUT);
+    circle(500, 400, ringIN);
+    circle(500, 400, ringOUT);
+}
+
+    //Ticks to check for user input
+function controlTick() {
+    if (keyIsDown(RIGHT_ARROW)) {
+        Cars[0].direction -= 5;
+    }
+    if (keyIsDown(LEFT_ARROW)) {
+        Cars[0].direction += 5;
+    }
+    if (keyIsDown(UP_ARROW)) {
+        Cars[0].engineForce = 10 * POP;
+    } else {
+        Cars[0].engineForce = 0;
+    }
 }
