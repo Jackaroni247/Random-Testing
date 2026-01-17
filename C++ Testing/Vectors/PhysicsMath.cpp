4c6d4dd354887e98f8e45b47f9fd1384fe87c65f
Jackaroni <121079788+Jackaroni247@users.noreply.github.com>
2026-01-17 07:47:51 -0600

    Fixed the main file to include and use the other headers

--- C++ Testing/Vectors/PhysicsMath.cpp
+++ C++ Testing/Vectors/PhysicsMath.cpp
@@ -1,20 +1,22 @@
-//GNU nano 8.1                                       PhysicsMath.cpp                                                 
-#include "MPVectorClass.h"
-#include "MatrixClass.h"
+include "MatrixClass.h"
 
 using namespace std;
 
 int main()
 {
 
-    MPVector v1("Engine", {1,2,3,4});
-    MPVector v2("Drag", {5,6,7,8});
+	MPVector v1("Engine", {1,2,3,4});
+	MPVector v2("Drag", {5,6,7,8});
 
-    v1.print();
-    v2.print();
-    addVectors(v1, v2).print();
-    subtractVectors(v1,v2).print();
-    averageVectors(v1,v2).print();
-    cout << "V1*V2: " << dotProduct(v1,v2) << '\n';
-    return 1;
+	Matrix m1("ED", {v1,v2});
+
+	m1.print();
+
+	v1.print();
+	v2.print();
+	addVectors(v1, v2).print();
+	subtractVectors(v1,v2).print();
+	averageVectors(v1,v2).print();
+	cout << "V1*V2: " << dotProduct(v1,v2) << '\n';
+	return 1;
 }
