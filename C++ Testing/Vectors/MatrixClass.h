d53bd262d5ed083a3193ffc951762022d19f4bb5
Jackaroni <121079788+Jackaroni247@users.noreply.github.com>
2026-01-17 07:46:50 -0600

    Created the base matrix class and added support for MPVectors

--- C++ Testing/Vectors/MatrixClass.h
+++ C++ Testing/Vectors/MatrixClass.h
@@ -1,11 +1,39 @@
 #include <iostream>
 #include <cmath>
 #include <string>
+#include "MPVectorClass.h"
 
 using namespace std;
 
 class Matrix {
+public:
+	string name;
+	vector<vector<float>> matrix;
+	int nRows;
+	int nCols;
 
+	Matrix(string n, vector<MPVector> m) {
+		name = n;
+		for(int i = 0; i < m.size(); i++) {
+			matrix.insert(matrix.end(), m[i].components);
+		}
+		nRows = matrix.size();
+		nCols = matrix[0].size();
+	}
 
+	Matrix(string n, vector<vector<float>> m) {
+		name = n;
+		matrix = m;
+	}
 
+	void print() {
+		cout << name << ": \n";
+		for(int i = 0; i < nRows; i++) {
+			cout << "| ";
+			for(int j = 0; j < nCols; j++) {
+				cout << matrix[i][j] << ' ';
+			}
+			cout << '|' << '\n';
+		}
+	}
 };
