4b5a511ff2b36f81ab9c1a790a5d095e2460338e
Jackaroni <121079788+Jackaroni247@users.noreply.github.com>
2026-01-20 22:35:43 -0600

    Started adding function to Matrix class

--- C++ Testing/Vectors/MatrixClass.h
+++ C++ Testing/Vectors/MatrixClass.h
@@ -7,33 +7,44 @@ using namespace std;
 
 class Matrix {
 public:
-	string name;
-	vector<vector<float>> matrix;
-	int nRows;
-	int nCols;
+       	string name;
+        vector<vector<float>> matrix;
+        int nRows;
+        int nCols;
 
-	Matrix(string n, vector<MPVector> m) {
-		name = n;
-		for(int i = 0; i < m.size(); i++) {
-			matrix.insert(matrix.end(), m[i].components);
-		}
-		nRows = matrix.size();
-		nCols = matrix[0].size();
-	}
+        Matrix(string n, vector<MPVector> m) {
+                name = n;
+                for(int i = 0; i < m.size(); i++) {
+                        matrix.insert(matrix.end(), m[i].components);
+                }
+                nRows = matrix.size();
+                nCols = matrix[0].size();
+        }
 
 	Matrix(string n, vector<vector<float>> m) {
-		name = n;
-		matrix = m;
-	}
+                name = n;
+                matrix = m;
+        }
 
 	void print() {
-		cout << name << ": \n";
-		for(int i = 0; i < nRows; i++) {
-			cout << "| ";
-			for(int j = 0; j < nCols; j++) {
-				cout << matrix[i][j] << ' ';
-			}
-			cout << '|' << '\n';
-		}
-	}
+                cout << name << ": \n";
+                for(int i = 0; i < nRows; i++) {
+                        cout << "| ";
+                        for(int j = 0; j < nCols; j++) {
+                                cout << matrix[i][j] << ' ';
+                        }
+                        cout << '|' << '\n';
+                }
+        }
 };
+
+Matrix addMatrices(Matrix m1, Matrix m2) {
+
+
+        return m1;
+}
+
+Matrix subtractMatrices(Matrix m1, Matrix m2) {
+
+        return m1;
+}
