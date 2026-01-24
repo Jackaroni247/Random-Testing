7b072bb0b2a94ae9245fbd4a59fc3d70bebfc66c
Jackaroni <121079788+Jackaroni247@users.noreply.github.com>
2026-01-23 23:31:43 -0600

    Fixed error in no return

--- C++ Testing/Vectors/MatrixClass.h
+++ C++ Testing/Vectors/MatrixClass.h
@@ -1,3 +1,4 @@
+#include <iostream> 
 #include <cmath>
 #include <string>
 #include "MPVectorClass.h"
@@ -6,42 +7,50 @@ using namespace std;
 
 class Matrix {
 public:
-       	string name;
-        vector<vector<float>> matrix;
-        int nRows;
-        int nCols;
+	string name;
+	vector<vector<float>> matrix;
+	int nRows;
+	int nCols;
 
-        Matrix(string n, vector<MPVector> m) {
-                name = n;
-                for(int i = 0; i < m.size(); i++) {
-                        matrix.insert(matrix.end(), m[i].components);
-                }
-                nRows = matrix.size();
-                nCols = matrix[0].size();
-        }
+	Matrix(string n, vector<MPVector> m) {
+		name = n;
+		for(int i = 0; i < m.size(); i++) {
+			matrix.insert(matrix.end(), m[i].components);
+		}
+		nRows = matrix.size();
+		nCols = matrix[0].size();
+	}
 
 	Matrix(string n, vector<vector<float>> m) {
-                name = n;
-                matrix = m;
-        }
+		name = n;
+		matrix = m;
+	}
 
 	void print() {
-                cout << name << ": \n";
-                for(int i = 0; i < nRows; i++) {
-                        cout << "| ";
-                        for(int j = 0; j < nCols; j++) {
-                                cout << matrix[i][j] << ' ';
-                        }
-                        cout << '|' << '\n';
-                }
-        }
+		cout << name << ": \n";
+		for(int i = 0; i < nRows; i++) {
+			cout << "| ";
+			for(int j = 0; j < nCols; j++) {
+				cout << matrix[i][j] << ' ';
+			}
+			cout << '|' << '\n';
+		}
+	}
 };
 
 Matrix addMatrices(Matrix m1, Matrix m2) {
-        if(m1.nRows!=m2.nRows || m1.nCols!=m2.nCols) {
-                cout << "Matrices are not of the same dimension" << '\n';
-                return m1;
+	//Checks to make sure theyre the same size;
+	if(m1.nRows!=m2.nRows || m1.nCols!=m2.nCols) {
+		cout << "Matrices are not of the same dimension" << '\n';
+		return m1;
+	}
+
+	for(int i = 0; i < m1.nRows; i++) {
+		for(int j = 0; j < m1.nCols; j++) {
+			
+               	}
         }
+	return m1;
 }
 
 Matrix subtractMatrices(Matrix m1, Matrix m2) {
@@ -49,5 +58,5 @@ Matrix subtractMatrices(Matrix m1, Matrix m2) {
                 cout << "Matrices are not of the same dimension" << '\n';
                 return m1;
         }
-        return m1;
+	return m1;
 }
