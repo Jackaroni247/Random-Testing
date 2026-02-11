4b1256fd7a7d344740dd0912b6fb7600b626a6b1
Jackaroni <121079788+Jackaroni247@users.noreply.github.com>
2026-02-10 22:21:59 -0600

    Started the brain neural network implementation

---
+++ Javascript Testing/P5.js Testing/Car Neural Network/carBrain.js
@@ -0,0 +1,20 @@
+class carBrain {
+
+    layerSize = 5;
+    numLayers = 3;
+
+    numOutputs = 3;
+    
+    layers = [];
+    weights = [];
+    outputs = [];
+
+    RELU(input) {
+        return Math.max(0,input)
+    }
+
+    propograte(r,dir,theta) {
+        
+    }
+
+}
